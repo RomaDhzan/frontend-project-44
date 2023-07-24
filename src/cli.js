3ff4db1221f556da0ace612570990fd147597caa
RomaDzhan <djanelidze122@gmail.com>
2023-07-24 08:30:36 +0300

    add line

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 export const greeting = () => {
-    const name = readlineSync.question('Your answer: ');
-    
-    console.log('Hello ' + name + '!');
+  const name = readlineSync.question('Your answer: ');
+
+  console.log(`Hello ${name}!`);
 };
